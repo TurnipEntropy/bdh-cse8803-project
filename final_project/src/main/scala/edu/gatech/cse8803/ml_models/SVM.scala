e9471eba4711c4a9680f0285cc8f396f5719df75
Nicholas Vercruysse <nicholas.vercruysse@gmail.com>
2018-04-19 20:24:59 -0400

    using full dataset, updated SVM

--- final_project/src/main/scala/edu/gatech/cse8803/ml_models/SVM.scala
+++ final_project/src/main/scala/edu/gatech/cse8803/ml_models/SVM.scala
@@ -6,23 +6,27 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.classification.{SVMModel, SVMWithSGD}
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
 import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.mllib.regression.LabeledPoint
 
 class SVM {
 
 
   var sqlContext: SQLContext = _
-  // def train(data: RDD[(KeyTuple, ValueTuple)]): SVMModel = {
-  //   val training = convertRDDtoDF(data)
-  //   SVMWithSGD.train(training)
-  // }
 
 
-  def convertRDDtoDF(data: RDD[(KeyTuple, ValueTuple)]) = {
+  def train(data: RDD[(KeyTuple, ValueTuple)]): SVMModel = {
+    val training = convertRDDtoLabeledPointRDD(data)
+    SVMWithSGD.train(training, 2)
+  }
+
+
+  def convertRDDtoLabeledPointRDD(data: RDD[(KeyTuple, ValueTuple)]): RDD[LabeledPoint] = {
     this.sqlContext = new SQLContext(data.context)
     val segmented = data.map({
-      case (k,v) => (v._2.toDouble, Vectors.dense(v._3.bpDia, v._3.bpSys, v._3.heartRate,
+      case (k,v) => new LabeledPoint(v._2.toDouble, Vectors.dense(v._3.bpDia, v._3.bpSys, v._3.heartRate,
                      v._3.respRate, v._3.temp, v._3.spo2, v._3.gcs, v._3.age.toDouble))
     })
-    this.sqlContext.createDataFrame(segmented).toDF("label", "features")
+    //this.sqlContext.createDataFrame(segmented).toDF("label", "features")
+    segmented
   }
 }
